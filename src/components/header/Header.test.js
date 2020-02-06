b70e489fced00ea9e56327f80e17fc72e7c13efa
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-05 17:50:10 -0800

    add snapshot test for Header using spyOn to mock useLocation

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
+import routeData from 'react-router';
+
 
 describe('Header component', () => {
+  const mockLocation = {
+    pathname: '/character',
+    hash: '',
+    search: '',
+    state: ''
+  };
+  
+  beforeEach(() => {
+    jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation);
+  });
+
   it('matches a snapshot', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toMatchSnapshot();
